439c634f31f063ccef18bacf7b2753231200c083
nishoni <80274164+nishoni@users.noreply.github.com>
2023-12-04 23:41:30 +0300

    Переезд на c8

---
+++ hashing_kursach/Core/Src/uart.c
@@ -0,0 +1,34 @@
+/*
+ * uart.c
+ *
+ *  Created on: 29 нояб. 2023 г.
+ *      Author: Pizza Delivery
+ */
+
+#include "uart.h"
+
+//int8_t UART2_rxBuffer[26] = {0};
+
+int uart_input()
+{
+//	int8_t message[35] = {'\0'};
+//	int8_t num = 0;
+//	char Message[] = "Write anything on Serial Terminal\r\n"; /* Message to be transmitted through UART */
+//
+//	HAL_Init();
+//
+//
+//	SystemClock_Config();
+//
+//	MX_GPIO_Init();
+//  	MX_USART2_UART_Init();
+//
+//  	HAL_UART_Transmit(&huart2, (int8_t *)Message, strlen(Message), 10);
+//  	HAL_UART_Receive_IT(&huart2, UART2_rxBuffer, 26);
+}
+
+//void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+//{
+//    HAL_UART_Transmit(&huart2, UART2_rxBuffer, 26, 100);
+//    HAL_UART_Receive_IT(&huart2, UART2_rxBuffer, 26);
+//}
